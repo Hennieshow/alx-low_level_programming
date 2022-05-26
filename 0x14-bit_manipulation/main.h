bc2e8faacf1c81ccb002ea997e98d65bc9df9a1b
Hennieshow <hennieshow@gmail.com>
2022-05-26 10:12:50 -0700

    Changes to main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef _BIT_MANIP_
 #define _BIT_MANIP_
 
+int _putchar(char c);
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
@@ -10,5 +11,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
