f464ec19c8eaaa21ae21bbf634820b27fdef2944
Hennieshow <hennieshow@gmail.com>
2022-07-12 15:23:19 -0700

    fix error

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Hennieshow/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp https://raw.github.com/Hennieshow/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/nrandom.so
